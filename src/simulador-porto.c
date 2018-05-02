4fd150d285084badc0c7daad47f4e28ae4d18d65
Mytsu <tlc.jooker@gmail.com>
2018-05-02 10:31:27 -0300

    implementando funções de alocação e liberação de memória

--- src/simulador-porto.c
+++ src/simulador-porto.c
@@ -16,27 +16,55 @@
 // setando quantidade máxima de unidades de tempo, para testes
 #define MAX_TEMPO 100
 
+typedef struct {
+    unsigned int id;
+    Lista * pilhas[4];
+} Navio;
+
 // filas de atracadouros
 Lista * fila_atracadouro[4];
 
+// ultimo id utilizado na criação de navios
+unsigned int last_id;
+
+// alocação/liberação de memória
 void alocar_navios(int qtd);
+void liberar_navio();
 void alocar_filas();
+void liberar_filas();
+void alocar_pilhas(Navio * nav);
+void liberar_pilhas(Navio * nav);
 
 int main(int args, char** argv) {
     // iniciando semente do gerador de numeros
-    srand(time(NULL));
-    alocar_filas();    
-
+    srand(time(NULL));    
+    alocar_filas();   
+    last_id = 0; 
     int tempo = 0;
     do {
 
         tempo++;
     } while(tempo < MAX_TEMPO);
-
+    liberar_filas();
 }
 
 void alocar_filas() {
     // iniciando filas de atracadouros
     for (int i = 0; i < 4; i++) 
         fila_atracadouro[i] = lista_nova();
+}
+
+void liberar_filas() {
+    for (int i = 0; i < 4; i++)
+        lista_destruir(fila_atracadouro[i]);
+}
+
+void alocar_pilhas(Navio * nav) {
+    for (int i = 0; i < 4; i++)
+        nav->pilhas[i] = lista_nova();
+}
+
+void liberar_pilhas(Navio * nav) {
+    for (int i = 0; i < 4; i++)
+        lista_destruir(nav->pilhas[i]);
 }
